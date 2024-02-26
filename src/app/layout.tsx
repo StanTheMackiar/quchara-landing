26d8391d5a8d7e95cdb8fcc825f057e66f5c361f
Stanly Calle <callestanly@gmail.com>
2024-02-26 00:29:59 -0500

    added seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { SITE_URL } from "@/helpers/env";
+import { COLORS } from "@/theme/colors";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -8,15 +10,40 @@ const myFont = localFont({
   display: "swap",
 });
 
+const TITLE = 'Quchara - Almuerzos a tiempo'
+const DESCRIPTION = "Programa tus almuerzos de la semana y recíbelos a una hora exacta."
+const IMAGE_URL = 'https://firebasestorage.googleapis.com/v0/b/quchara-ed80e.appspot.com/o/Quchara%2Ficon_orange.png?alt=media&token=2f30d087-4b86-42b9-8b95-73f843d764e5'
+
 export const metadata: Metadata = {
-  title: "Quchara - Almuerzos a tiempo",
-  description: "Programa tus almuerzos de la semana y recíbelos a una hora exacta.",
+  title: TITLE,
+  description: DESCRIPTION,
   category: 'Restaurante',
-  keywords: ['Quchara', 'Comida', 'Restaurante', 'Bebida', 'Domicilio', 'Puntualidad', 'Cuchara', 'Almuerzo'],
+  keywords: ['Quchara', 'Comida', 'Puntualidad', 'Restaurante', 'Bebida', 'Domicilio', 'Puntualidad', 'Cuchara', 'Almuerzo'],
+  twitter: {
+    title: TITLE,
+    card: 'summary_large_image',
+    site: TITLE,
+  },
   robots: "index, follow",
   authors: {
     name: 'Quchara S.A.S.'
-  }
+  },
+  openGraph: {
+    title: TITLE,
+    description: DESCRIPTION,
+    type: 'website',
+    url: SITE_URL,
+    siteName: TITLE,
+    locale: 'es_ES',
+    images: [
+      {
+        url: IMAGE_URL,
+        width: 512,
+        height: 512,
+        alt: 'Logo Quchara'
+      }
+    ]
+  },
 };
 
 export default function RootLayout({
@@ -26,6 +53,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <meta
+          key='apple-mobile-web-app-status-bar-style'
+          name='apple-mobile-web-app-status-bar-style'
+          content={COLORS.brand}
+        />
+        <meta
+          key='msapplication-navbutton-color'
+          name='msapplication-navbutton-color'
+          content={COLORS.brand}
+        />
+         <meta key='msapplication-TileColor' name='msapplication-TileColor' content={COLORS.brand} />
+         <meta key='theme-color' name='theme-color' content={COLORS.brand} />
+      </head>
       <body className={myFont.variable}>
         {children}
       </body>
--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,2 +1,4 @@
 export const SERVER_URL_PROD = process.env.NEXT_PUBLIC_SERVER_URL_PROD;
 export const WAIT_TIME = process.env.NEXT_PUBLIC_WAIT_TIME;
+
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
