7a87e2a40ba22058544f5255404edcd261c7f498
Stanly Calle <callestanly@gmail.com>
2024-03-07 11:28:06 -0500

    responsive adjust

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useResponsive } from "@/hooks";
 import { ContentHero } from "./ContentHero";
 
 const Hero = () => {
-  const { isMobile, isTablet, isDesktop } = useResponsive();
 
   return (
     <section className="flex flex-col md:flex-row gap-4 md:gap-0 w-screen relative overflow-hidden  max-w-mw-container">
@@ -12,13 +10,12 @@ const Hero = () => {
       </div>
       <div className="flex-1 flex items-center justify-center container md:pr-9 lg:pr-0 pointer-events-none pb-2">
         <video
-          width={isMobile ? 250 : isTablet ? 300 : isDesktop ? 450 : undefined}
           height={350}
           src="/images/hero/cellphones.webm"
           loop
           autoPlay
           muted
-          className="h-[350px] md:h-[600px] lg:h-max"
+          className="w-[250px] sm:w-[300px] md:w-[350px] lg:w-[450px] xl:w-[450px] 2xl:w-max"
         />
       </div>
     </section>
